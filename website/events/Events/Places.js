eaf59f7a09aec8556ad9c2807a9ac100a8bfa7f9
Joyeeta Pal <joyeetapal26@gmail.com>
2018-03-12 13:09:35 +0530

    Added files

---
+++ website/events/Events/Places.js
@@ -0,0 +1,10 @@
+function doFirst()
+{
+		content=document.getElementById('a1');
+		
+}
+function display1()
+{
+	
+}
+window.addEventListener('load',doFirst,false);
---
+++ website/events/Events/index.php
@@ -0,0 +1,6 @@
+<?php
+	$no1=21;
+	$no2=22;
+	$res=no1+no2;
+	echo $res;
+?>
